ef79e76fc8dde100a7ca74f0becbb6988429c0d4
chocoscoding <chocoscoding@gmail.com>
2023-08-01 00:09:18 +0100

    navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="text-2xl font-medium z-20 relative">
+      <ul className="flex z-5 gap-4 relative text-black">
+        <Link
+          href={`/`}
+          className="mr-3">
+          <li>Home</li>
+        </Link>
+        <Link
+          href={`/Contact`}
+          className="mr-3">
+          <li>Contact</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+}
